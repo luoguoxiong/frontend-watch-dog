bb342347bf701f709607e14b419e4d2ac93176bc
luoguoxiong <luoguoxiong@ipin.com>
2019-06-23 01:04:43 +0800

    添加缓存数据

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/common.scss';
-import './assets/reset.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/common.scss'
+import './assets/reset.css'
 import './config/rem'
-import App from './page/App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import App from './page/App'
+import * as serviceWorker from './serviceWorker'
+import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import { Provider } from 'react-redux';
-import Reducers from './redux/reducers';
-import rootSaga from './redux/sagas';
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(Reducers, applyMiddleware(sagaMiddleware));
+import { Provider } from 'react-redux'
+import Reducers from './redux/reducers'
+import rootSaga from './redux/sagas'
+
+const LoadData = newVersion => {
+  const easyMarket_version =
+    JSON.parse(localStorage.getItem('easyMarket_version')) || ''
+  let initData = undefined
+  if (newVersion === easyMarket_version) {
+    initData = JSON.parse(localStorage.getItem('easyMarket_initData'))
+  } else {
+    localStorage.setItem('easyMarket_version', newVersion)
+  }
+  return initData
+}
+const data = LoadData(3)
+console.log(data)
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(Reducers, data, applyMiddleware(sagaMiddleware))
+
 sagaMiddleware.run(rootSaga)
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-  , document.getElementById('root'));
-serviceWorker.unregister();
+  </Provider>,
+  document.getElementById('root')
+)
+
+window.onbeforeunload = () => {
+  const state = store.getState()
+  localStorage.setItem('easyMarket_initData', JSON.stringify(state))
+}
+
+serviceWorker.unregister()
--- src/page/topic/index.js
+++ src/page/topic/index.js
@@ -1,29 +1,26 @@
-import React, {Component, Fragment} from 'react'
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
-import *as topicAction from '../../redux/actions/topic'
-import LazyLoad from 'react-lazyload';
-import {Link} from 'react-router-dom'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as topicAction from '../../redux/actions/topic'
+import LazyLoad from 'react-lazyload'
+import { Link } from 'react-router-dom'
 import ImgLazyLoad from '../../components/imgLazyLoad'
 import './index.css'
-class Topic extends Component{
+class Topic extends Component {
   componentWillMount() {
-    this.props.actions.fetchData({page:1, size:100})
+    this.props.actions.fetchData({ page: 1, size: 100 })
   }
   render() {
-    const {topicData} = this.props
+    const { topicData } = this.props
     return (
       <Fragment>
-        {topicData.map((item) => (
+        {topicData.map(item => (
           <LazyLoad throttle={200} height={300} key={item.id}>
             <Link className="topicItem" to={`/topicDetail/${item.id}`}>
-                <ImgLazyLoad
-                  offSetTop={44}
-                  realUrl = {item.scene_pic_url}>
-                </ImgLazyLoad>
-                <div className="topicItemTitle">{item.title}</div>
-                <div className="topicItemSubtitle">{item.subtitle}</div>
-                <div className="topicItemPrice">{item.price_info}元起</div>
+              <ImgLazyLoad offSetTop={44} realUrl={item.scene_pic_url} />
+              <div className="topicItemTitle">{item.title}</div>
+              <div className="topicItemSubtitle">{item.subtitle}</div>
+              <div className="topicItemPrice">{item.price_info}元起</div>
             </Link>
           </LazyLoad>
         ))}
@@ -31,6 +28,11 @@ class Topic extends Component{
     )
   }
 }
-const mapStateToProps = (state, props) => ({...state.topic})
-const mapDispatchToProps = dispatch => ({actions: bindActionCreators(topicAction, dispatch)})
-export default connect(mapStateToProps, mapDispatchToProps)(Topic)
+const mapStateToProps = (state, props) => ({ ...state.topic })
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(topicAction, dispatch)
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Topic)
